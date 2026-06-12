1959965a5d7bc8f3267f33b7ca3845128b2dbd36
vsnorek <vaclav.snorek@gmail.com>
2026-06-12 22:19:04 +0200

    chore: use emdash cloudflare worker entrypoint

--- src/worker.ts
+++ src/worker.ts
@@ -1,3 +1,1 @@
-import handler from "@astrojs/cloudflare/entrypoints/server";
-export { PluginBridge } from "@emdash-cms/cloudflare/sandbox";
-export default handler;
+export { default, PluginBridge } from "@emdash-cms/cloudflare/worker";
